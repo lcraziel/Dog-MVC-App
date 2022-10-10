55944dd25fbaef757cd13f4c1ae541c33f1f356b
Luis hdz <antunez_chely@hotmail.com>
2022-10-10 16:31:50 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            /*DogViewModel doggo = new DogViewModel()
+            { Name = "Rick", Age = 7 };*/
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+            //return View("Index");
+        }
+        public string Hello()
+        {
+            return "who's there?";
+        }
+    }
+}
